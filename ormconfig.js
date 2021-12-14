2e8875bbe36eb305c07f4a751f9f635bcf8e79bc
Filipe Gutierry <filipegutierry@MacBook-Air-de-Filipe.local>
2021-12-14 19:36:20 -0300

    chore: dotenv config

---
+++ ormconfig.js
@@ -0,0 +1,30 @@
+const productionEnv = {
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    entities: ['./dist/entities/*.js'],
+    migrations: ['./dist/database/migrations/*.js'],
+    cli: {
+      migrationsDir: './src/database/migrations',
+    },
+    ssl:
+      process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : false,
+  };
+  
+  const developmentEnv = {
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    entities: ['./src/entities/*.ts'],
+    migrations: ['./src/database/migrations/*.ts'],
+    cli: {
+      migrationsDir: './src/database/migrations',
+    },
+    ssl:
+      process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : false,
+  };
+  
+  module.exports =
+    process.env.NODE_ENV === 'production' ? productionEnv : developmentEnv;
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+
 import "reflect-metadata";
 
 import express from "express";
@@ -6,11 +8,13 @@ import "./database";
 
 import { routes } from "./routes";
 
+dotenv.config();
+
 const app = express();
 
 app.use(express.json());
 
-app.use(routes); 
+app.use(routes);
 
 app.listen(3000, () => {
   console.log("Server is running at port 3000");
